12e2125bcea58eaab8a6d1223bab6819ec2d6369
Blane <blane.cordes@gmail.com>
2012-07-15 21:13:32 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dww2::Application.config.secret_token = '1d358da5cb0eca2af90bed11da99a0e5656630eceaac0d1130161796e41d31913ed590d571b4be565d1902473dbfcabe1d5b4b253bfff10f9f55ddbed986e70b'
