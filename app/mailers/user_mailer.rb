cd6073f3d27753fb1bfc3d8d11784a161b32d5d2
Blane <blane.cordes@gmail.com>
2012-07-28 00:02:29 -0400

    mailer working on heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
  
   def welcome_email(user)
     @user = user
-    @url  = "http://example.com/login"
-    mail(:to => user.email, :subject => "Welcome to My Awesome Site")
+    @url  = "www.dailywaiverwire.com"
+    mail(:to => user.email, :subject => "Wecome to the Daily Waiver Wire")
   end
 end
