41e439f51513afaeb5fecd8bfcbf31eba6d88df4
Blane <blane.cordes@gmail.com>
2013-01-03 13:04:46 -0500

    tweaked player_feed migration

--- db/migrate/20130102023028_adding_columns_to_player_feeds.rb
+++ db/migrate/20130102023028_adding_columns_to_player_feeds.rb
@@ -1,7 +1,7 @@
 class AddingColumnsToPlayerFeeds < ActiveRecord::Migration
   def change
-    add_column :player_feeds, :guid, :string
-    add_column :player_feeds, :url, :string
-    add_column :player_feeds, :published_at, :datetime
+    add_column :player_feed, :guid, :string
+    add_column :player_feed, :url, :string
+    add_column :player_feed, :published_at, :datetime
   end
 end
