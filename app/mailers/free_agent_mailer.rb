ec385e7b04578ad2ada009b371b36378e56e8ebe
Blane <blane.cordes@gmail.com>
2013-01-06 00:50:08 -0500

    fixed regex again, confirmed feed items are properly being associated to players, need to work on mailer notifications for production

--- app/mailers/free_agent_mailer.rb
+++
@@ -1,9 +0,0 @@
-class FreeAgentMailer < ActionMailer::Base
-  default :from => "dailywaiverwire@gmail.com"
- 
-  def free_agent_email(user)
-    @user = user
-    @url  = "www.dailywaiverwire.com"
-    mail(:to => user.email, :subject => "Today's Waiver Wire")
-  end
-end
