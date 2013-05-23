f6e7285a1b1ab1eabf65cef44791a148f249750d
Blane <blane.cordes@gmail.com>
2013-05-23 00:29:37 -0400

    add sports data 2013 roster file for seeding

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+require "/Users/blanecordes/dww2/db/sports_data_rosters.rb"
+
 # Initialize the rails application
 Dww2::Application.initialize!
 
