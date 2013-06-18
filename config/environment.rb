bd9073429fd9ef37012dd3570518b751e73f5f19
Blane <blane.cordes@gmail.com>
2013-06-17 23:59:28 -0400

    fix config

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require "/Users/blanecordes/dww2/db/sports_data_rosters.rb"
+require "#{Rails.root}/db/sports_data_rosters.rb"
 
 # Initialize the rails application
 Dww2::Application.initialize!
